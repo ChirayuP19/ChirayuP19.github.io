d04143938cf2e1e794ba1ad39098af465c9b1e4b
chirayu <chirayu.patel1@dcmail.ca>
2024-08-03 22:21:55 -0400

    Update Result function
    - Update result function

--- Assignment 4/Part1/main.js
+++ Assignment 4/Part1/main.js
@@ -19,30 +19,29 @@ const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewal
 
 randomize.addEventListener('click', result);
 
-
 function result() {
-    let newStory = storyText;
-  
-    const xItem = randomValueFromArray(insertX);
-    const yItem = randomValueFromArray(insertY);
-    const zItem = randomValueFromArray(insertZ);
-  
-    newStory = newStory.replaceAll(':insertx:',xItem);
-    newStory = newStory.replaceAll(':inserty:',yItem);
-    newStory = newStory.replaceAll(':insertz:',zItem);
-  
-    if (customName.value !== '') {
-      const name = customName.value;
-      newStory = newStory.replaceAll('Bob', name);
-    }
-  
-    if (document.getElementById("uk").checked) {
-      const weight = ${Math.round(300*0.0714286)} stone;
-      const temperature =  ${Math.round((94-32) * 5 / 9)} centigrade;
-      newStory = newStory.replaceAll('94 fahrenheit', temperature);
-      newStory = newStory.replaceAll('300 pounds', weight);
-    }
-  
-    story.textContent = newStory;
-    story.style.visibility = 'visible';
+  let newStory = storyText;
+
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replaceAll(':insertx:',xItem);
+  newStory = newStory.replaceAll(':inserty:',yItem);
+  newStory = newStory.replaceAll(':insertz:',zItem);
+
+  if (customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replaceAll('Bob', name);
   }
+
+  if (document.getElementById("uk").checked) {
+    const weight = `${Math.round(300*0.0714286)} stone`;
+    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+    newStory = newStory.replaceAll('94 fahrenheit', temperature);
+    newStory = newStory.replaceAll('300 pounds', weight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
